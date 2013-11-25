779c73c9289494a267e07a8fbcb70157afd43e73
cesar <julio.nt@outlook.com>
2013-11-25 01:28:17 -0200

    desenvolvimento do menu superior

---
+++ Includes/menu_superior.php
@@ -0,0 +1,31 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="../CSS/menu_superior.css" />
+<nav id="menu_superior_centralizado"><nav id="menu_superior">
+	<ul>
+		<li><a href='#'>ARTIGOS</a>
+			<ul>
+				<li><a href='#'>Para iniciantes</a></li>
+				<li><a href='#'>Para iniciados</a></li>
+				<li><a href='#'>Tira dúvidas</a></li>
+				<li><a href='#'>Diversos</a></li>
+			</ul>
+		</li>
+		<li><a href='#'>NOTÍCIAS </a>
+			<ul>
+				<li><a href='#'>Recentes</a></li>
+				<li><a href='#'>Mais lidas</a></li>
+			</ul>
+		</li>
+		<li><a href='#'>ANÁLISES</a>
+			<ul>
+				<li><a href='#'>Hardware</a></li>
+				<li><a href='#'>Software</a></li>
+				<li><a href='#'>Jogos</a></li>
+			</ul>
+		</li>
+		<li><a href='#'>TUTORIAIS</a></li>
+		<li><a href='#'>PODCAST</a></li>
+		<li><a href='#'>FÓRUM</a></li>
+		<li><a href='#'>BUSCA</a></li>
+	</ul>
+</nav></nav>
